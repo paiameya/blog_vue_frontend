f29a7708fc75fa7574480f2794359f2155cb7b4e
harshal <harshal@experience.com>
2021-05-10 10:13:23 -0400

    "changed Component name from ProfilePictures to Avatar"

---
+++ src/components/Avatar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <img class="img-content" :src="AvatarURL" alt="Avatar" />
+  </div>
+</template>
+
+<script>
+import { ref, onBeforeMount } from 'vue'
+export default {
+  name: 'Avatar',
+  props: {
+    url: String,
+  },
+  setup(props) {
+    let AvatarURL = ref('')
+    onBeforeMount(async () => {
+      AvatarURL.value = props.url
+    })
+    return {
+      AvatarURL,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.img-content {
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+}
+</style>
