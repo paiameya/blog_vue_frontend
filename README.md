9b8e41925e2d62e2f15678409283c0c51b458488
paiameya <ameya@rayabharitech.com>
2021-05-10 11:22:13 +0530

    formatted code and install eslint and prettier

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
-# blog_vue_frontend
-
-## Project setup
-```
 npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+npm prepare
+npx husky add .husky/pre-push "npm run lint && npm run pretty"
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import PrimeVue from 'primevue/config'
 
-createApp(App).use(store).use(router).mount("#app");
+import 'primevue/resources/themes/saga-blue/theme.css' //theme
+import 'primevue/resources/primevue.min.css' //core css
+import 'primeicons/primeicons.css' //icons
+const app = createApp(App).use(store).use(router).mount('#app')
+app.use(PrimeVue)
