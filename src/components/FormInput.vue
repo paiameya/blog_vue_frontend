fef03ca73dc147c524bba367597970ede8b55aae
arunmg83 <arun.mg@rayabharitech.com>
2021-05-11 10:42:34 +0530

    Search Long component added using two way binding

---
+++ src/components/FormInput.vue
@@ -0,0 +1,40 @@
+<template>
+  <InputText
+    :type="type"
+    :placeholder="placeholder"
+    :value="value"
+    :class="className"
+    @input="$emit('update:value', $event.target.value)"
+  />
+</template>
+
+<script>
+import InputText from 'primevue/inputtext'
+
+export default {
+  name: 'form-input',
+  components: {
+    InputText,
+  },
+  props: {
+    type: String,
+    placeholder: { type: String, default: '' },
+    value: { type: String, default: undefined },
+    className: { type: String, default: '' },
+  },
+}
+</script>
+
+<style scoped>
+.p-inputtext {
+  border: 0px;
+  box-shadow: none !important;
+  border-bottom: 1px solid #ced4da;
+  width: 100%;
+}
+.p-inputtext:focus {
+  border: 0px;
+  box-shadow: none;
+  border-bottom: 1px solid #2196f3;
+}
+</style>
---
+++ src/components/SearchLong.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <form-input
+      :type="inputType"
+      :placeholder="searchPlaceholder"
+      :value="searchValue"
+      v-model="searchValue"
+      :className="`p-inputtext-lg`"
+    />
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+import FormInput from './FormInput.vue'
+export default {
+  components: {
+    FormInput,
+  },
+  setup() {
+    let searchValue = ref('')
+    let searchPlaceholder = ref('Enter the text')
+    let inputType = ref('search')
+    function onSubmit() {
+      //TODO: need to call search API.
+      console.log('====>', searchValue.value)
+    }
+    return { searchValue, searchPlaceholder, inputType, onSubmit }
+  },
+}
+</script>
