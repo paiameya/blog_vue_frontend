140bf5c9134ab0683de6bc1eea9ab593c3c120ce
vinayp95 <vinayrpattan@gmail.com>
2021-05-12 14:35:33 +0530

    BlogCategoryList

---
+++ src/BlogCategoryList/BlogCategoryList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>
+      <BlogCategory
+        v-for="categoryName in categoriesList"
+        :key="categoryName.id"
+        :category="categoryName"
+      />
+    </div>
+  </div>
+</template>
+<script>
+import { ref, onBeforeMount } from 'vue'
+import BlogCategory from '@/BlogCategory/BlogCategory'
+export default {
+  name: 'BlogCategoryList',
+  components: {
+    BlogCategory,
+  },
+  props: {
+    categories: Array,
+    // categories: { Type: Array, default: () => [] },
+  },
+    setup(props) {
+      const categoriesList = ref([])
+      onBeforeMount(() => {
+        categoriesList.value = props.categories
+      })
+      return {
+        categoriesList,
+      }
+    },
+}
+</script>
