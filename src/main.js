fdaa6700f2bcea05b65f33c878c6b577bdfbc9ab
vinayp95 <vinayrpattan@gmail.com>
2021-05-12 12:04:05 +0530

    removed unwanted imports in main.js

--- src/main.js
+++ src/main.js
@@ -3,11 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import PrimeVue from 'primevue/config'
-import Tag from 'primevue/tag'
 import 'primevue/resources/themes/saga-blue/theme.css' //theme
 import 'primevue/resources/primevue.min.css' //core css
 import 'primeicons/primeicons.css' //icons
 const app = createApp(App).use(store).use(router).mount('#app')
 app.use(PrimeVue)
-
-app.component('Tag', Tag)
