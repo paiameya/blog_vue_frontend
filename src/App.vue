a507330a6a31c2146648987c1250fe97e11ce71b
vinayp95 <vinayrpattan@gmail.com>
2021-05-13 17:54:30 +0530

    landing page

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,42 @@
 <template>
   <div id="nav">
+    
     <router-view />
   </div>
+  <LandingPage />
 </template>
+<script>
+import LandingPage from './views/LandingPage'
+export default {
+  components: {
+    LandingPage
+  }
+}
+</script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Forum&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
-
-body {
-  font-size: calc(14px + (26 - 14) * ((100vw - 300px) / (1600 - 300)));
+body,html {
+  margin: 0px;
+  padding: 0px;
+  display:block;
+  width:100%;
+  min-height:100vh;
+  overflow-x: hidden;
 }
-
 #app {
+  position: relative;
+  width: 100%;
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
 }
 
-#nav {
+/* #nav {
   padding: 30px;
-}
+} */
 
 #nav a {
   font-weight: bold;
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <Header />
+        <div class="scro">
+            
+        </div>
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+export default {
+
+    components: {
+        Header,
+        Footer,
+    }
+
+}
+</script>
+<style scoped>
+.container{
+    display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+.scro{
+    flex: 1;
+}
+/* .scro {
+    overflow-y: auto;
+}
+.container {
+    height: 100%;
+}
+Header{
+    position: absolute;
+    width: 100%;
+    top: 0;
+}
+Footer {
+ position: absolute;
+ bottom: 0;
+ width: 100%;
+
+} */
+</style>
