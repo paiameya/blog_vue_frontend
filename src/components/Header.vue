8337033731e9ed668891b7dd08c09dc54ff566f4
harshal <harshal@experience.com>
2021-05-13 00:53:07 -0400

    "header which accepts the logo and searchbar component"

---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="header">
+    <img class="logo-content" :src="LogoURL" alt="Logo" />
+    <input class="right" type="text" placeholder="Search" />
+  </div>
+</template>
+
+<script>
+import { ref, onBeforeMount } from 'vue'
+export default {
+  name: 'Header',
+  props: {
+    logoURL: String,
+  },
+  setup(props) {
+    const LogoURL = ref('')
+    onBeforeMount(() => {
+      LogoURL.value = props.logoURL
+    })
+    return {
+      LogoURL,
+    }
+  },
+}
+</script>
+
+<style scoped>
+#header {
+  display: flex;
+  justify-content: space-between;
+  background-color: var(--surface-00) !important;
+  opacity: 0.5;
+  padding: 20px;
+}
+#header img.logo-content {
+  width: 10%;
+  height: 1.75em;
+  opacity: 1 !important;
+}
+#header input.right {
+  width: 15% !important;
+}
+</style>
