30498f39031f1df6f786e29308e3b6bc6dfb5b38
harshal <harshal@experience.com>
2021-05-14 07:34:31 -0400

    "AutherPage view created"

--- src/views/AuthorPage.vue
+++ src/views/AuthorPage.vue
@@ -1,26 +1,46 @@
 <template>
-  <div>
-    <Header
-      logoURL="http://2.bp.blogspot.com/-tRdfCkaPO0A/Ui-PRJLDZlI/AAAAAAABMI8/8L9maIUroq8/s640/google-new-logo.png"
-    />
-    <div class="card">
-      <AuthorCard
-        imageURL="https://picsum.photos/200/300"
-        authorName="John Bordowski"
-        authorTitle="How did it end"
-      />
+  <div class="container">
+    <Header logoURL="" :Name="authorDetails.name" />
+    <div class="authorPost">
+      <div class="card">
+        <AuthorCard
+          imageURL="https://miro.medium.com/fit/c/131/131/1*xK5kJEka8T-IfgxeG-evKg.jpeg"
+          :authorName="authorDetails.name"
+          authorTitle="How did it end"
+        />
+      </div>
     </div>
+    <h1 v-for="blog in authorDetails.blogs" :key="blog.id">{{ blog.title }}</h1>
+    <Footer class="footer" />
   </div>
 </template>
 
 <script>
-import Header from '../components/Header'
-import AuthorCard from '../components/AuthorCard.vue'
+import Header from '@/components/Header'
+import AuthorCard from '@/components/AuthorCard.vue'
+import Footer from '@/components/Footer.vue'
+import { fetchAuthorDetails } from '../services/authors/fetchAuthorDetails'
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
 export default {
   name: 'AuthorPage',
   components: {
     AuthorCard,
     Header,
+    Footer,
+  },
+  setup() {
+    const authorDetails = ref([])
+    const route = useRoute()
+    onMounted(async () => {
+      const response = await fetchAuthorDetails(route.params.id)
+      authorDetails.value = response.data
+      console.log(authorDetails)
+      console.log(typeof authorDetails.value.name)
+    })
+    return {
+      authorDetails,
+    }
   },
 }
 </script>
@@ -30,5 +50,16 @@ export default {
   display: flex;
   justify-content: center;
   margin-top: 20px;
+}
+.authorPost {
+  flex: 1;
+}
+.footer {
+  width: 100%;
+}
+.container {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
 }
 </style>
