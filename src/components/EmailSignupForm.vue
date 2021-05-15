431a478a0ce3af6d91341a38c1931a4d4639b5d5
Shreya Swaroop <shreyaswrp@gmail.com>
2021-05-15 12:11:01 +0530

    add: email sign up form componenet

---
+++ src/components/EmailSignupForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="form-container">
+    <form class="email-form">
+      <div class="fname">
+        <label>Full Name</label>
+        <input type="text" name="fname" value="" />
+      </div>
+      <div class="email">
+        <label>Email Address</label>
+        <input type="text" name="email" value="" />
+      </div>
+      <div class="password">
+        <label>Password</label>
+        <input type="text" name="password" value="" />
+      </div>
+      <div class="confirm-password">
+        <label>Confirm Password</label>
+        <input type="text" name="confirmpassword" value="" />
+      </div>
+      <input type="submit" value="Sign Up" />
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.email-form {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  width: 18em;
+  margin: auto;
+  padding: 2em;
+  font-family: arial;
+}
+
+.email-form > div {
+  margin-bottom: 2em;
+}
+
+.fname {
+  margin-bottom: 1em;
+}
+</style>
