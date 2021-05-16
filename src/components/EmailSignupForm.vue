321a145e10a9d73e8c3151f9dbd3fe37002801a1
Shreya Swaroop <shreyaswrp@gmail.com>
2021-05-16 13:29:21 +0530

    refactor: email  sign up form

--- src/components/EmailSignupForm.vue
+++ src/components/EmailSignupForm.vue
@@ -7,26 +7,22 @@
       <div>
         <h2>Full Name</h2>
         <input
-          id="fname"
           name="fname"
           type="text"
           class="input-field"
           v-model="fullName"
-          :class="{ active: isValid }"
-          @change="handleChange"
+          :class="{ active: isFullNameValid }"
         />
       </div>
 
       <div>
         <h2>Email</h2>
         <input
-          id="email"
           name="email"
           type="text"
           class="input-field"
           v-model="email"
-          :class="{ active: isValid }"
-          @change="handleChange"
+          :class="{ active: isEmailValid }"
         />
         <span v-if="emailError">{{ emailError }}</span>
       </div>
@@ -34,13 +30,11 @@
       <div>
         <h2>Password</h2>
         <input
-          id="password"
           name="password"
           type="text"
           class="input-field"
           v-model="password"
-          :class="{ active: isValid }"
-          @change="handleChange"
+          :class="{ active: isPasswordValid }"
         />
         <span v-if="passwordError">{{ passwordError }}</span>
       </div>
@@ -48,17 +42,15 @@
       <div>
         <h2>Confirm Password</h2>
         <input
-          id="confirmpassword"
           name="confirmpassword"
           type="text"
           class="input-field"
           v-model="confirmPassword"
-          :class="{ active: isValid }"
-          @change="handleChange"
+          :class="{ active: isConfirmPasswordValid }"
         />
         <span v-if="confirmPasswordError">{{ confirmPasswordError }}</span>
       </div>
-      <div>
+      <div class="sign-up-button">
         <button class="btn" type="submit" @click="validateForm">Sign Up</button>
       </div>
     </form>
@@ -70,7 +62,10 @@ import { ref } from 'vue'
 export default {
   name: 'EmailSignupForm',
   setup() {
-    const isValid = ref(false)
+    const isFullNameValid = ref(false)
+    const isEmailValid = ref(false)
+    const isPasswordValid = ref(false)
+    const isConfirmPasswordValid = ref(false)
     const fullName = ref('')
     const email = ref('')
     const password = ref('')
@@ -78,29 +73,38 @@ export default {
     const emailError = ref('')
     const passwordError = ref('')
     const confirmPasswordError = ref('')
-    // const emailRegex = ref('')
-    // emailRegex.value =
-    //   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    const emailRegex = ref('')
+    emailRegex.value = /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
     const passwordRegex = ref('')
-    passwordRegex.value =
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,12}$/
+    passwordRegex.value = new RegExp(
+      '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,12})'
+    )
+
     const validateForm = e => {
+      isFullNameValid.value = false
+      isEmailValid.value = false
+      isPasswordValid.value = false
+      isConfirmPasswordValid.value = false
+      confirmPasswordError.value = ''
+      emailError.value = ''
+      passwordError.value = ''
+
       if (!fullName.value) {
-        isValid.value = true
+        isFullNameValid.value = true
       }
       if (!email.value) {
-        isValid.value = true
+        isEmailValid.value = true
       }
       if (!password.value) {
-        isValid.value = true
+        isPasswordValid.value = true
       }
       if (!confirmPassword.value) {
-        isValid.value = true
+        isConfirmPasswordValid.value = true
       }
-      // if (emailRegex.value.test(email)) {
-      //   emailError.value = 'Email address is of an invalid format'
-      // }
-      if (passwordRegex.value.test(password)) {
+      if (!emailRegex.value.test(email.value)) {
+        emailError.value = 'Email address is of an invalid format'
+      }
+      if (!passwordRegex.value.test(password.value)) {
         passwordError.value =
           'Password must have 1 upper case, 1 lower case, 1 number and 1 special character. Password must be between 8 - 12 characters'
       }
@@ -109,22 +113,12 @@ export default {
       }
       e.preventDefault()
     }
-    const onChange = () => {
-      if (fullName.value) {
-        isValid.value = false
-      }
-      if (email.value) {
-        isValid.value = false
-      }
-      if (password.value) {
-        isValid.value = false
-      }
-      if (confirmPassword.value) {
-        isValid.value = false
-      }
-    }
+
     return {
-      isValid,
+      isFullNameValid,
+      isEmailValid,
+      isPasswordValid,
+      isConfirmPasswordValid,
       validateForm,
       fullName,
       email,
@@ -133,7 +127,6 @@ export default {
       emailError,
       passwordError,
       confirmPasswordError,
-      onChange,
     }
   },
 }
@@ -142,7 +135,8 @@ export default {
 <style scoped>
 .email-form {
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  width: 22em;
+  width: 20em;
+  max-height: 41em;
   margin: auto;
   padding: 2em;
   font-family: arial;
@@ -164,6 +158,10 @@ export default {
 }
 
 .title {
+  text-align: center;
+}
+
+.sign-up-button {
   text-align: center;
 }
 </style>
