079f79b612f773a292b180e26afabf13a560aa49
vinayp95 <vinayrpattan@gmail.com>
2021-05-17 12:46:20 +0530

    removed unwanted style

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@
 <style>
 body,html {
   margin: 0;
-  overflow-x: hidden;
 }
 @import url('https://fonts.googleapis.com/css2?family=Forum&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
