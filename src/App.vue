5c17b1748b8586a5bd2356e8dc925ca40a6db7f3
paiameya <55129904+paiameya@users.noreply.github.com>
2021-05-17 12:51:53 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Forum&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
 body {
   margin: 0;
   padding: 0;
 }
-@import url('https://fonts.googleapis.com/css2?family=Forum&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
 </style>
